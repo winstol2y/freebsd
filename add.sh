60bb478249fe24d939ba4dd0ce9c9f3b0dcb5221
WinStol2y <ratchanon@winstol2y1.local>
2015-06-23 11:35:03 +0700

    update_dhcp

--- add.sh
+++ add.sh
@@ -5,24 +5,10 @@ name=`cat /Applications/XAMPP/xamppfiles/htdocs/name_add.txt`
 host=`cat /Applications/XAMPP/xamppfiles/htdocs/host_add.txt`
 
 remove='#'$macAddress
-toFile=/Applications/XAMPP/xamppfiles/htdocs/1.txt 
+toFile=/usr/local/etc/dhcpd.conf 
                                                           
-echo "	group {															$remove" >> $toFile 
-echo "	   use-host-decl-name on;										$remove" >> $toFile
-echo "																	$remove" >> $toFile
-echo "	  host $name;														$remove" >> $toFile
-echo "	     hardward ethernet $macAddress;						$remove" >> $toFile
-echo "	     fixed-address $host;								$remove" >> $toFile
-echo "	   }															$remove" >> $toFile
-echo "	}																$remove" >> $toFile
-echo "    is equivalent to												$remove" >> $toFile
 echo "    	  host $name {												$remove" >> $toFile
 echo "    	     hardware ethernet $macAddress;					$remove" >> $toFile
 echo "      	     fixed-address $host;							$remove" >> $toFile
-echo "   	     option host-name  "'"'"$name"'";'"									$remove" >> $toFile
 echo "	}																$remove" >> $toFile
-echo "																	$remove" >> $toFile
-echo "																	$remove" >> $toFile
-echo "																	       " >> $toFile
-echo "																		   " >> $toFile
 echo "$macAddress ,$name,$host" >> list.txt
